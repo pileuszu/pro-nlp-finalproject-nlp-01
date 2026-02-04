129aaac1d858a6894036ca7046befae85fedabab
pileuszu <pileuszu@gmail.com>
2026-02-05 01:15:48 +0900

    style: revert branding font to ClimateCrisisKorean (Black weight)

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -58,7 +58,7 @@ const config: Config = {
                 sm: 'calc(var(--radius) - 4px)'
             },
             fontFamily: {
-                blackhan: ['"Black Han Sans"', 'sans-serif'],
+                climate: ['ClimateCrisisKorean', 'sans-serif'],
             }
         }
     },
